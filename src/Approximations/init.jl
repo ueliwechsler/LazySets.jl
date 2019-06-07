bb02dc93852a3cf804f3ff4297e89edca68cba02
schillic <schillic@informatik.uni-freiburg.de>
2019-06-07 09:08:00 +0200

    add optional IntervalMatrices import

--- src/Approximations/init.jl
+++ src/Approximations/init.jl
@@ -1,7 +1,12 @@
 function __init__()
     @require TaylorModels = "314ce334-5f6e-57ae-acf6-00b6e903104a" load_taylormodels()
+    @require IntervalMatrices = "9f9a964d-5e3a-583f-8a9b-b3cc3f5bd869" load_intervalmatrices()
 end
 
 function load_taylormodels()
     eval(load_taylormodels_overapproximation())
+end
+
+function load_intervalmatrices()
+    eval(load_intervalmatrices_overapproximation())
 end
